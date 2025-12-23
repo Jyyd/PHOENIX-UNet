16732ed9e8c4231fd3eb7b7ab1266928ac73280d
yudie jianyao <78450036+Jyyd@users.noreply.github.com>
2025-12-23 15:42:31 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import sys, os
 os.chdir(sys.path[0])
 import yaml
-import train_phen
-import test_phen
+import train_phoenix
+import test_phoenix
 
 def load_config(path):
     with open(path, 'r', encoding='utf-8') as f:
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cfg = load_config('./dataset/config_files/config_example.yaml')
     gpu_id = 0
     print(f"Trying on GPU {gpu_id} ...")
-    train_phen.main(cfg, gpu_id)
+    train_phoenix.main(cfg, gpu_id)
     print("Training completed. Starting testing ...")
-    test_phen.main(cfg, gpu_id)
+    test_phoenix.main(cfg, gpu_id)
 
